809be5ee592a8bc4f1b1f7794df7ceac916d906f
Petar <petarburic554@gmail.com>
2025-05-20 00:36:16 +0200

    Supabase client configuration

---
+++ src/lib/supabase.js
@@ -0,0 +1,30 @@
+import { createClient } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-url-polyfill/auto';
+
+// SecureStore adapter for Supabase auth
+const ExpoSecureStoreAdapter = {
+  getItem: (key) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key, value) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+// Supabase configuration
+const supabaseUrl = '**';
+const supabaseAnonKey = '***';
+
+// create Supabase client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
